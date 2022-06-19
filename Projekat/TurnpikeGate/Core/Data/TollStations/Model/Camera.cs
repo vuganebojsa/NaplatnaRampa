ec173ec4fb5a6ce9110c4d3b2e9ed6fca02a65ea
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2022-06-19 23:59:14 +0200

    Implemented a few models

---
+++ Projekat/TurnpikeGate/Core/Data/TollStations/Model/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnpikeGate.Core.Data.TollStations.Model
+{
+    class Camera
+    {
+        [BsonElement("_id")]
+        public ObjectId ID { get; set; }
+        [BsonElement("toolBoothId")]
+        public ObjectId ToolBoothId { get; set; }
+
+        public Camera(ObjectId toolBoothId)
+        {
+            ToolBoothId = toolBoothId;
+        }
+    }
+}
---
+++ Projekat/TurnpikeGate/Core/Data/TollStations/Model/Semaphore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnpikeGate.Core.Data.TollStations.Model
+{
+    class Semaphore
+    {
+        [BsonElement("_id")]
+        public ObjectId ID { get; set; }
+        [BsonElement("toolBoothId")]
+        public ObjectId ToolBoothId { get; set; }
+
+        public Semaphore(ObjectId toolBoothId)
+        {
+            ToolBoothId = toolBoothId;
+        }
+    }
+}
