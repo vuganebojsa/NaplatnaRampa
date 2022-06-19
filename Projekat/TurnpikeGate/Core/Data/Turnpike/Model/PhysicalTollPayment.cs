b71b2797804aa3cb4027c18a4b0e5dadfe9d82b7
Ognjen Radovanovic <ognjen34car@gmail.com>
2022-06-19 21:52:09 +0200

    Added TollPayment class and repository

---
+++ Projekat/TurnpikeGate/Core/Data/Turnpike/Model/PhysicalTollPayment.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnpikeGate.Core.Data.Turnpike.Model
+{
+    public class PhysicalTollPayment : TollPayment
+    {
+        public ObjectId ReferentId { get; set; }
+
+        public PhysicalTollPayment(string registrationPlate, DateTime enteranceTime, DateTime exitTime, 
+                                   ObjectId entranceStationId, ObjectId referentId)
+        { 
+            ID = ObjectId.GenerateNewId();
+            RegistrationPlate = registrationPlate;
+            EntranceTime = enteranceTime;
+            ExitTime = exitTime;
+            EntranceStationId = entranceStationId;
+            ReferentId = referentId;
+
+        }
+
+    }
+}
