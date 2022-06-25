9e86233a9e38be36af4c8652deac50b6c6ee328e
Ognjen Radovanovic <ognjen34car@gmail.com>
2022-06-25 16:37:33 +0200

    Implemented states for ramp

--- Projekat/TurnpikeGate/Core/TollStations/Model/Ramp.cs
+++ Projekat/TurnpikeGate/Core/TollStations/Model/Ramp.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TurnpikeGate.Core.TollStations.States;
 
 namespace TurnpikeGate.Core.TollStations.Model
 {
@@ -15,10 +16,20 @@ namespace TurnpikeGate.Core.TollStations.Model
         [BsonElement("toolBoothId")]
         public ObjectId ToolBoothId { get; set; }
 
+        [BsonElement("state")]
+        public State State { get; set; }
+
         public Ramp(ObjectId toolBoothId)
         {
             ID = ObjectId.GenerateNewId();
             ToolBoothId = toolBoothId;
+            State = new Lowered(this);
+        }
+
+        public void ChangeState(State state) 
+        {
+            State = state;
+        
         }
     }
 }
---
+++ Projekat/TurnpikeGate/Core/TollStations/States/Lowered.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurnpikeGate.Core.TollStations.Model;
+
+namespace TurnpikeGate.Core.TollStations.States
+{
+    internal class Lowered : State
+    {
+        public Lowered(Ramp ramp) : base(ramp)
+        {
+        }
+
+        public override void Do()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
