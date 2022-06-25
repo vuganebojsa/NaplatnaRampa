07d1dde84ddff8dabd242897dd341bf55240c017
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-06-25 18:06:40 +0200

    Fixed CRUD for tollbooths
    Added so that it can now see if the devices are functional or not

--- Projekat/TurnpikeGate/View/TagSellerViews/TagSellerMainForm.cs
+++ Projekat/TurnpikeGate/View/TagSellerViews/TagSellerMainForm.cs
@@ -13,11 +13,12 @@ namespace TurnpikeGate.View.TagSellerViews
 {
     public partial class TagSellerMainForm : Form
     {
-        ILoginService _loginService;
+        private readonly ILoginService _loginService;
 
         public TagSellerMainForm(ILoginService loginService)
         {
             InitializeComponent();
+            _loginService = loginService
         }
 
         private void btnTags_Click(object sender, EventArgs e)
