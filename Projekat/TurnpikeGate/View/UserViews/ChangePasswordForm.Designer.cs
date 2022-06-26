1eb00e68d68fe66a6d7afa04ac749c13ad25489c
tince250 <t.s.mihajlovic25@gmail.com>
2022-06-26 22:40:29 +0200

    More final touches

--- Projekat/TurnpikeGate/View/UserViews/ChangePasswordForm.Designer.cs
+++ Projekat/TurnpikeGate/View/UserViews/ChangePasswordForm.Designer.cs
@@ -33,16 +33,18 @@
             this.tbPassword = new System.Windows.Forms.TextBox();
             this.tbRepeatedPass = new System.Windows.Forms.TextBox();
             this.btnConfirm = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label1.Location = new System.Drawing.Point(140, 36);
+            this.label1.Location = new System.Drawing.Point(141, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(380, 64);
+            this.label1.Size = new System.Drawing.Size(530, 92);
             this.label1.TabIndex = 0;
             this.label1.Text = "Izmenite inicijalnu lozinku koju \r\nVam je dodelio admin:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -53,32 +55,28 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label2.Location = new System.Drawing.Point(65, 132);
+            this.label2.Location = new System.Drawing.Point(81, 141);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(526, 25);
+            this.label2.Size = new System.Drawing.Size(632, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "Izmenom inicijalne lozinke aktivirate nalog za dalje korisenje.";
             // 
             // tbPassword
             // 
             this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbPassword.Location = new System.Drawing.Point(188, 185);
-            this.tbPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbPassword.Location = new System.Drawing.Point(246, 242);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.PasswordChar = '*';
-            this.tbPassword.PlaceholderText = "nova lozinka";
-            this.tbPassword.Size = new System.Drawing.Size(275, 32);
+            this.tbPassword.Size = new System.Drawing.Size(314, 38);
             this.tbPassword.TabIndex = 2;
             // 
             // tbRepeatedPass
             // 
             this.tbRepeatedPass.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbRepeatedPass.Location = new System.Drawing.Point(188, 256);
-            this.tbRepeatedPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbRepeatedPass.Location = new System.Drawing.Point(246, 336);
             this.tbRepeatedPass.Name = "tbRepeatedPass";
             this.tbRepeatedPass.PasswordChar = '*';
-            this.tbRepeatedPass.PlaceholderText = "ponovite novu lozinku";
-            this.tbRepeatedPass.Size = new System.Drawing.Size(275, 32);
+            this.tbRepeatedPass.Size = new System.Drawing.Size(314, 38);
             this.tbRepeatedPass.TabIndex = 3;
             // 
             // btnConfirm
@@ -87,26 +85,46 @@
             this.btnConfirm.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnConfirm.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnConfirm.ForeColor = System.Drawing.Color.White;
-            this.btnConfirm.Location = new System.Drawing.Point(217, 330);
-            this.btnConfirm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConfirm.Location = new System.Drawing.Point(303, 419);
             this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(184, 44);
+            this.btnConfirm.Size = new System.Drawing.Size(210, 59);
             this.btnConfirm.TabIndex = 4;
             this.btnConfirm.Text = "POTVRDI";
             this.btnConfirm.UseVisualStyleBackColor = false;
             this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(246, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "nova lozinka:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(246, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "ponovite lozinku:";
+            // 
             // ChangePasswordForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 398);
+            this.ClientSize = new System.Drawing.Size(785, 531);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnConfirm);
             this.Controls.Add(this.tbRepeatedPass);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ChangePasswordForm";
             this.Text = "ChangePasswordForm";
             this.ResumeLayout(false);
@@ -121,5 +139,7 @@
         private TextBox tbPassword;
         private TextBox tbRepeatedPass;
         private Button btnConfirm;
+        private Label label3;
+        private Label label4;
     }
 }
