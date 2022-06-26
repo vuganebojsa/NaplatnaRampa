02b37b3850041f3704ab9219b4fd4234bb440be1
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-06-26 20:27:44 +0200

    Slight gui changes

--- Projekat/TurnpikeGate/View/TagSellerViews/TagSellerForm.Designer.cs
+++ Projekat/TurnpikeGate/View/TagSellerViews/TagSellerForm.Designer.cs
@@ -54,10 +54,9 @@
             // 
             this.tbID.Enabled = false;
             this.tbID.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbID.Location = new System.Drawing.Point(123, 141);
-            this.tbID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbID.Location = new System.Drawing.Point(108, 106);
             this.tbID.Name = "tbID";
-            this.tbID.Size = new System.Drawing.Size(286, 38);
+            this.tbID.Size = new System.Drawing.Size(251, 32);
             this.tbID.TabIndex = 0;
             // 
             // label1
@@ -65,9 +64,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label1.Location = new System.Drawing.Point(151, 47);
+            this.label1.Location = new System.Drawing.Point(132, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 38);
+            this.label1.Size = new System.Drawing.Size(171, 30);
             this.label1.TabIndex = 1;
             this.label1.Text = "DOPUNA TAGA";
             // 
@@ -76,9 +75,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(27, 150);
+            this.label2.Location = new System.Drawing.Point(24, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 31);
+            this.label2.Size = new System.Drawing.Size(74, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "ID Taga";
             // 
@@ -87,9 +86,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label3.Location = new System.Drawing.Point(27, 232);
+            this.label3.Location = new System.Drawing.Point(24, 174);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 31);
+            this.label3.Size = new System.Drawing.Size(79, 25);
             this.label3.TabIndex = 3;
             this.label3.Text = "Kolicina";
             // 
@@ -97,10 +96,9 @@
             // 
             this.tbAmount.Enabled = false;
             this.tbAmount.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbAmount.Location = new System.Drawing.Point(123, 225);
-            this.tbAmount.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbAmount.Location = new System.Drawing.Point(108, 169);
             this.tbAmount.Name = "tbAmount";
-            this.tbAmount.Size = new System.Drawing.Size(286, 38);
+            this.tbAmount.Size = new System.Drawing.Size(251, 32);
             this.tbAmount.TabIndex = 4;
             // 
             // btnAddFunds
@@ -112,22 +110,20 @@
             this.btnAddFunds.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnAddFunds.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnAddFunds.ForeColor = System.Drawing.Color.White;
-            this.btnAddFunds.Location = new System.Drawing.Point(123, 396);
-            this.btnAddFunds.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAddFunds.Location = new System.Drawing.Point(108, 297);
             this.btnAddFunds.Name = "btnAddFunds";
-            this.btnAddFunds.Size = new System.Drawing.Size(286, 71);
+            this.btnAddFunds.Size = new System.Drawing.Size(250, 53);
             this.btnAddFunds.TabIndex = 8;
-            this.btnAddFunds.Text = "Dodaj sredstva";
+            this.btnAddFunds.Text = "DODAJ SREDSTVA";
             this.btnAddFunds.UseVisualStyleBackColor = false;
             this.btnAddFunds.Click += new System.EventHandler(this.btnAddFunds_Click);
             // 
             // chbAddFounds
             // 
             this.chbAddFounds.AutoSize = true;
-            this.chbAddFounds.Location = new System.Drawing.Point(127, 68);
-            this.chbAddFounds.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chbAddFounds.Location = new System.Drawing.Point(111, 51);
             this.chbAddFounds.Name = "chbAddFounds";
-            this.chbAddFounds.Size = new System.Drawing.Size(18, 17);
+            this.chbAddFounds.Size = new System.Drawing.Size(15, 14);
             this.chbAddFounds.TabIndex = 9;
             this.chbAddFounds.UseVisualStyleBackColor = true;
             this.chbAddFounds.CheckedChanged += new System.EventHandler(this.cbAddFounds_CheckedChanged);
@@ -135,10 +131,9 @@
             // chbRegisterTag
             // 
             this.chbRegisterTag.AutoSize = true;
-            this.chbRegisterTag.Location = new System.Drawing.Point(128, 63);
-            this.chbRegisterTag.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chbRegisterTag.Location = new System.Drawing.Point(112, 47);
             this.chbRegisterTag.Name = "chbRegisterTag";
-            this.chbRegisterTag.Size = new System.Drawing.Size(18, 17);
+            this.chbRegisterTag.Size = new System.Drawing.Size(15, 14);
             this.chbRegisterTag.TabIndex = 10;
             this.chbRegisterTag.UseVisualStyleBackColor = true;
             this.chbRegisterTag.CheckedChanged += new System.EventHandler(this.cbRegisterTag_CheckedChanged);
@@ -148,9 +143,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label4.Location = new System.Drawing.Point(160, 41);
+            this.label4.Location = new System.Drawing.Point(140, 31);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(251, 38);
+            this.label4.Size = new System.Drawing.Size(194, 30);
             this.label4.TabIndex = 11;
             this.label4.Text = "IZDAVANJE TAGA";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -169,10 +164,9 @@
             this.panel1.Controls.Add(this.tbName);
             this.panel1.Controls.Add(this.chbRegisterTag);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(494, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Location = new System.Drawing.Point(432, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(746, 761);
+            this.panel1.Size = new System.Drawing.Size(653, 571);
             this.panel1.TabIndex = 12;
             // 
             // label8
@@ -180,9 +174,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label8.Location = new System.Drawing.Point(54, 396);
+            this.label8.Location = new System.Drawing.Point(47, 297);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(66, 28);
+            this.label8.Size = new System.Drawing.Size(53, 21);
             this.label8.TabIndex = 17;
             this.label8.Text = "Valuta";
             // 
@@ -190,10 +184,9 @@
             // 
             this.cbCurrency.Enabled = false;
             this.cbCurrency.FormattingEnabled = true;
-            this.cbCurrency.Location = new System.Drawing.Point(54, 439);
-            this.cbCurrency.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbCurrency.Location = new System.Drawing.Point(47, 329);
             this.cbCurrency.Name = "cbCurrency";
-            this.cbCurrency.Size = new System.Drawing.Size(66, 28);
+            this.cbCurrency.Size = new System.Drawing.Size(58, 23);
             this.cbCurrency.TabIndex = 16;
             // 
             // label5
@@ -201,9 +194,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label5.Location = new System.Drawing.Point(65, 303);
+            this.label5.Location = new System.Drawing.Point(57, 227);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 31);
+            this.label5.Size = new System.Drawing.Size(38, 25);
             this.label5.TabIndex = 13;
             this.label5.Text = "Tip";
             // 
@@ -212,9 +205,9 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label6.Location = new System.Drawing.Point(59, 225);
+            this.label6.Location = new System.Drawing.Point(52, 169);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 31);
+            this.label6.Size = new System.Drawing.Size(80, 25);
             this.label6.TabIndex = 14;
             this.label6.Text = "Prezime";
             // 
@@ -223,9 +216,9 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.label7.Location = new System.Drawing.Point(59, 150);
+            this.label7.Location = new System.Drawing.Point(52, 112);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(52, 31);
+            this.label7.Size = new System.Drawing.Size(43, 25);
             this.label7.TabIndex = 15;
             this.label7.Text = "Ime";
             // 
@@ -238,12 +231,11 @@
             this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnRegister.ForeColor = System.Drawing.Color.White;
-            this.btnRegister.Location = new System.Drawing.Point(160, 396);
-            this.btnRegister.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRegister.Location = new System.Drawing.Point(140, 297);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(286, 71);
+            this.btnRegister.Size = new System.Drawing.Size(250, 53);
             this.btnRegister.TabIndex = 13;
-            this.btnRegister.Text = "Izdavanje";
+            this.btnRegister.Text = "IZDAVANJE";
             this.btnRegister.UseVisualStyleBackColor = false;
             this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
@@ -252,38 +244,35 @@
             this.cbType.Enabled = false;
             this.cbType.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.cbType.FormattingEnabled = true;
-            this.cbType.Location = new System.Drawing.Point(160, 295);
-            this.cbType.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbType.Location = new System.Drawing.Point(140, 221);
             this.cbType.Name = "cbType";
-            this.cbType.Size = new System.Drawing.Size(286, 39);
+            this.cbType.Size = new System.Drawing.Size(251, 33);
             this.cbType.TabIndex = 13;
             // 
             // tbLastName
             // 
             this.tbLastName.Enabled = false;
             this.tbLastName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbLastName.Location = new System.Drawing.Point(160, 223);
-            this.tbLastName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbLastName.Location = new System.Drawing.Point(140, 167);
             this.tbLastName.Name = "tbLastName";
-            this.tbLastName.Size = new System.Drawing.Size(286, 38);
+            this.tbLastName.Size = new System.Drawing.Size(251, 32);
             this.tbLastName.TabIndex = 13;
             // 
             // tbName
             // 
             this.tbName.Enabled = false;
             this.tbName.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbName.Location = new System.Drawing.Point(160, 143);
-            this.tbName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbName.Location = new System.Drawing.Point(140, 107);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(286, 38);
+            this.tbName.Size = new System.Drawing.Size(251, 32);
             this.tbName.TabIndex = 14;
             // 
             // TagSellerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1031, 675);
+            this.ClientSize = new System.Drawing.Size(902, 506);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.chbAddFounds);
             this.Controls.Add(this.btnAddFunds);
@@ -293,7 +282,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbID);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TagSellerForm";
             this.Text = "TagSellerForm";
             this.Load += new System.EventHandler(this.TagSellerForm_Load);
